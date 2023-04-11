643a5f494fd072cf387d61f8ff0ec0a4cd0e6df9
eakse <84707480+eakse@users.noreply.github.com>
2023-04-11 10:44:59 +0200

    Add project files.

---
+++ EdoAkse.ZoomMax/ZoomMax.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+
+namespace EdoAkse.ZoomMax
+{
+
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string pluginGuid = "edoakse.starvalor.maxzoom";
+        public const string pluginName = "EdoAkse Max Zoom";
+        public const string pluginVersion = "1.0.0";
+
+
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Main));
+        }
+
+
+        [HarmonyPatch(typeof(CameraControl))]
+        [HarmonyPatch("SetupCamera")]
+        [HarmonyPatch("Update")]
+        [HarmonyPatch("AdjustCamera")]
+        [HarmonyPatch("LateUpdate")]
+        [HarmonyPrefix]
+        private static void ZoomFix_Pre(ref int ___maxZoomLevel, ref float[] ___zoomScale)
+        {
+            // This number is the max zoomlevel. Basically the max index for the zoomScale array
+            // maxZoomLevel used in two methods:
+            // Update
+            // AdjustCamera
+            ___maxZoomLevel = 7;
+            // ZoomScale is the list of floats actually containing the zooms
+            // it is used in the following methods:
+            // SetupCamera
+            // Update
+            // LateUpdate
+            // AdjustCamera
+            ___zoomScale = new float[]
+            {
+                60f,
+                120f,
+                210f,
+                360f,
+                600f,
+                850f,
+                1000f,
+                1250f,
+                1600f
+            };
+        }
+    }
+}
+
